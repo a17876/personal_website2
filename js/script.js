80da1ba5d60b3d0dd6bf06a9aeadbb1a426d3125
Kaylyn <a17876@naver.com>
2021-03-31 16:01:00 -0700

    js - added loop and if statement to check userinput and push it to array

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-
 let courseList = [
     { code: "ACIT 1515", name: "Scripting for IT" },
     { code: "ACIT 1420", name: "Introduction to System Administration" },
     { code: "COMM 1116", name: "Business Communications" },
 ];
+
 let courseNum;
 do {
-    courseNum = prompt("Enter the course code (e.g. 1350) : ");
-}
-while (!isNaN(courseNum) === false);
-for(let item of courseList)
-{
-    if(item.code.includes(courseNum) === true && courseNum.length === 4)
-    {
-        console.log(`Yes. I am taking the course: ${courseList["code"]} - ${courseList["name"]}`);
-    }
-    else{
-        courseList.push({ code: "ACIT ____", name: null });
-        console.log(`Course ${courseNum} is successfully added`)
-    }
+    courseNum = prompt("Enter the 4-digit course number(e.g. 1420): ")
+} while (!isNaN(courseNum) === false || (isNaN(courseNum) === false && !courseNum.length === 4));
+
+let resultIncludes = true;
+let index = null;
+for (let i = 0; i < 3; ++i){
+    if (courseList[i].code.includes(courseNum)) {
+        index = i;
+        console.log(`Yes. I am taking the course: ${courseList[i].code} - ${courseList[i].name}`);
+    } else {
+        resultIncludes = false;
+    };
+};
+
+if (resultIncludes === false){
+  courseList.push({ code: `ACIT ${courseNum}`, name: null})
+  alert('suceed')
 }
