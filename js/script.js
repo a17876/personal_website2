2ca1b8f36546d72e187d1a607cb87e028d5ec5c5
Kaylyn <a17876@naver.com>
2021-03-31 18:16:21 -0700

    js - correct loop: course exist still show the success message

--- js/script.js
+++ js/script.js
@@ -9,18 +9,14 @@ do {
     courseNum = prompt("Enter the 4-digit course number(e.g. 1420): ")
 } while (isNaN(courseNum) === true || isNaN(courseNum) === false && !(courseNum.length === 4));
 
-let resultIncludes = true;
-let index = null;
+let resultIncludes = false;
 for (let i = 0; i < 3; ++i){
     if (courseList[i].code.includes(courseNum)) {
-        index = i;
-        console.log(`Yes. I am taking the course: ${courseList[i].code} - ${courseList[i].name}`);
-    } else {
-        resultIncludes = false;
-    };
+      console.log(`Yes. I am taking the course: ${courseList[i].code} - ${courseList[i].name}`);
+      resultIncludes = true;
+    }
 };
-
 if (resultIncludes === false){
-  courseList.push({ code: `ACIT ${courseNum}`, name: null})
-  alert(`ACIT ${courseNum} is added successfully!`)
-}
+  courseList.push({ code: `ACIT ${courseNum}`, name: null});
+  alert(`ACIT ${courseNum} is added successfully!`);
+};
